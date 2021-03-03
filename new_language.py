4f0853abc5dd2579300642f96d7c3d8c683dde83
kaution b <kaution.burrowes@gmail.com>
2021-03-03 19:57:00 +1300

    new_language v1.4

--- new_language.py
+++ new_language.py
@@ -18,7 +18,7 @@ vowels = {
     "ø̌": "oh",
     "ǔ": "aow",
     "ǎ": "ow",
-    "ǒ": "ou",
+    "ǒ": "ou"
 }
 
 consonants = {
@@ -39,17 +39,23 @@ consonants = {
     "tj": "tch",
     "v": "f",
     "w": "v",
-    "z": "z",
+    "z": "z"
 }
 
-number_of_words = int(input("How many words do you want to generate? "))
-
-formula_list = list(input("""[Formula Legend]
-v = vowel
-c = consonant
-
-Example: cvc
-What formula should the words follow? """).strip().lower())
+formulas = [
+    # wøt
+    list("cvc"), 
+    # wyjø
+    list("cvcv"), 
+    # iøg
+    list("vvc"), 
+    # ǎidø̌
+    list("vvcv"), 
+    # nyǎtø
+    list("cvvcv"), 
+    # ydusj
+    list("vcvc")
+]
 
 def add_random_letter(letter_dict):
     random_num = random.random()
@@ -63,11 +69,47 @@ def add_random_letter(letter_dict):
     word_list.append(word_letter)
     pro_word_list.append(pro_letter)
 
+number_of_words = int(input("""How many words do you want to generate?\n>> """))
+
+formula_yn = print("""Would you like to use a custom formula, or use the default ones?
+(1) Custom Formula
+(2) Default Formulas""")
+
+loop = True
+formula_list = []
+
+while loop:
+    formula_yn = input(">> ").strip().lower()
+    
+    if formula_yn == "1":
+        formula_list.append(list(input("""[Formula Legend]
+v = vowel
+c = consonant
+
+Example: cvc
+What formula should the words follow?
+>> """).strip().lower()))
+        break
+    
+    elif formula_yn == "2":
+        for i in range(number_of_words):
+            word_list = []
+            pro_word_list = []
+            
+            formula_list.append(random.choice(formulas))
+        break
+    
+    else:
+        print("That's not an option.")
+        continue
+
+# Seems ot only print 3 elements
 for i in range(number_of_words):
+    chosen_formula_list = random.choice(formula_list)
     word_list = []
     pro_word_list = []
     
-    for letter in formula_list:
+    for letter in chosen_formula_list:
         if letter == "v": 
             add_random_letter(vowels)
         elif letter == "c": 
