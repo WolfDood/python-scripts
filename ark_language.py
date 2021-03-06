4a773c3489a84eb8ab4d059dd825d9e2ca71070c
kaution b <kaution.burrowes@gmail.com>
2021-03-06 19:32:10 +1300

    Create ark_language.py

---
+++ ark_language.py
@@ -0,0 +1,107 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  1 17:23:58 2021
+@author: https://github.com/kaubu
+"""
+
+import random
+
+vowels = {
+	"u": "a",
+	"y": "ee",
+	"o": "aw",
+	"a": "æ",
+	"ø": "ir",
+	"i": "i",
+	"e": "e",
+}
+
+consonants = {
+	"b": "b",
+	"d": "d",
+	"'dj": "j",
+	"f": "f",
+	"h": "h",
+	"j": "y",
+	"k": "k",
+	"l": "l",
+	"m": "m",
+	"n": "n",
+	"s": "s",
+	"v": "v",
+	"g": "g",
+	"r": "r",
+	"'sj": "'zh",
+	"w": "w"
+}
+
+formulas = [
+	list("cvc"), 
+	list("cvcv"), 
+	list("vvc"), 
+	list("vvcv"), 
+	list("cvvcv"), 
+	list("vcvc")
+]
+
+def add_random_letter(letter_dict):
+	random_num = random.random()
+
+	random.seed(random_num)
+	word_letter = random.choice(list(letter_dict.keys()))
+
+	random.seed(random_num)
+	pro_letter = random.choice(list(letter_dict.values()))
+
+	word_list.append(word_letter)
+	pro_word_list.append(pro_letter)
+
+number_of_words = int(input("""How many words do you want to generate?\n>> """))
+
+formula_yn = print("""Would you like to use a custom formula, or use the default ones?
+(1) Custom Formula
+(2) Default Formulas""")
+
+loop = True
+formula_list = []
+
+while loop:
+	formula_yn = input(">> ").strip().lower()
+
+	if formula_yn == "1":
+		formula_list.append(list(input("""[Formula Legend]
+v = vowel
+c = consonant
+Example: cvc
+What formula should the words follow?
+>> """).strip().lower()))
+		break
+
+	elif formula_yn == "2":
+		for i in range(number_of_words):
+			word_list = []
+			pro_word_list = []
+
+			formula_list.append(random.choice(formulas))
+		break
+
+	else:
+		print("That's not an option.")
+		continue
+
+# Seems ot only print 3 elements
+for i in range(number_of_words):
+	chosen_formula_list = random.choice(formula_list)
+	word_list = []
+	pro_word_list = []
+
+	for letter in chosen_formula_list:
+		if letter == "v": 
+			add_random_letter(vowels)
+		elif letter == "c": 
+			add_random_letter(consonants)
+		else:
+			word_list.append(letter)
+			pro_word_list.append(letter)
+
+	print("Word {}: {}; Pronunciation: {}".format(i+1, "".join(word_list), "".join(pro_word_list)))
