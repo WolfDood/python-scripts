7b6d6568dc486e3c1e232a746f1ad11f3535a625
kaution b <kaution.burrowes@gmail.com>
2021-04-15 17:14:46 +1200

    v1.0.0

---
+++ new_untitled_language.py
@@ -0,0 +1,47 @@
+ipa = {
+	# Special
+	" ": " ",
+	"'": "", # Placeholder character, has no sound
+	"`": "", # Alt placeholder character
+	"‘": "", # Second verse, same as the first
+	# Consonants
+	"b": "b",
+	"d": "d",
+	"t": "t",
+	"g": "g",
+	"k": "k",
+	"j": "ʒ", # Vi -si- on
+	"s": "s",
+	"m": "m",
+	"n": "n",
+	"f": "f",
+	"v": "v",
+	"r": "r", # r is rolled r, upside down r is -r- ed
+	"z": "z",
+	"l": "l",
+	"p": "p",
+	"h": "h",
+	# Vowels
+	"a": "ɑ",
+	"e": "ɛ", # b -e- d
+	"o": "o",
+	"i": "ɪ", # b -i- t
+	"y": "i", # ee
+	"u": "u",
+}
+
+if __name__ == "__main__":
+	print("New Untitled Language Translator v1.0.0")
+	while True:
+		sentence = input("Please enter a sentence to translate from New Untitled Language to IPA. Type \":exit\" to exit.\n>> ")
+		if sentence == ":exit": break
+
+		translated = []
+		for char in list(sentence):
+			if char not in ipa:
+				print(f"Invalid character: \"{char}\"")
+				continue
+			translated.append(ipa[char])
+		
+		result = "".join(translated)
+		print(f"Result:\n{result}")
